5a4d96dd755774e64be76c4095a1437115c5eb28
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2025-11-01 15:02:47 +0530

    custom basic Authntication

--- src/main/java/com/example/microservices/config/SecurityConfig.java
+++ src/main/java/com/example/microservices/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.example.microservices.config;
 
+import com.example.microservices.service.CustomUserServiceDetails;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +22,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 
-                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/h2-console").permitAll()
+                        .anyRequest().authenticated())
+
                 .httpBasic(Customizer.withDefaults());
-//                .anyRequest("/h2-console/**").permitAll() // Allow access to H2 console
+//                .anyRequest("/h2-console/**").permitAll() ;// Allow access to H2 console
 //                .anyRequest().authenticated()
 //                .and()
 //                .csrf()
@@ -27,6 +37,26 @@ public class SecurityConfig {
 //                .frameOptions().sameOrigin(); // Allow H2 console to load in a frame
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomUserServiceDetails();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder){
+
+        DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(daoAuthenticationProvider);
     }
 
 }
--- src/main/java/com/example/microservices/model/User.java
+++ src/main/java/com/example/microservices/model/User.java
@@ -2,11 +2,16 @@ package com.example.microservices.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +19,39 @@ public class User {
 
     private String name;
 
+    private String password;
+
+    private String role;
+
     private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
