0e67be58b7fb14d89f7b05d73feecbd98b8f6111
MANOJGUJJALA <34826798+MANOJGUJJALA@users.noreply.github.com>
2025-11-02 15:53:25 +0530

    AWS S3 Upload/Download File

--- src/main/java/com/example/microservices/config/SecurityConfig.java
+++ src/main/java/com/example/microservices/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers("/download/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jWtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
